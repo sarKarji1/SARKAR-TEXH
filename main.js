7e08fc14685412f30b04161cba9e892983cd377f
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2024-08-24 14:54:31 +0500

    Update main.js

--- main.js
+++ main.js
@@ -176,11 +176,11 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
 			await delay(1999)
             console.log(chalk.yellow(`\n\n                  ${chalk.bold.blue(`[ ${botname} ]`)}\n\n`))
             console.log(chalk.cyan(`< ================================================== >`))
-	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: Tech God`))
-            console.log(chalk.magenta(`${themeemoji} GITHUB: techgod143`))
-            console.log(chalk.magenta(`${themeemoji} INSTAGRAM: @techgod143 `))
+	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: Sarkarji`))
+            console.log(chalk.magenta(`${themeemoji} GITHUB: sarKarji1`))
+            console.log(chalk.magenta(`${themeemoji} INSTAGRAM: @bandaheali `))
             console.log(chalk.magenta(`${themeemoji} WA NUMBER: ${owner}`))
-            console.log(chalk.magenta(`${themeemoji} CREDIT: tech god sir`))
+            console.log(chalk.magenta(`${themeemoji} CREDIT: Bandah e ali`))
         }
         if (
             connection === "close" &&
